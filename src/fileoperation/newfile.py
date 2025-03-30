cb68f1849f66450c0cfba873eb9e21aeb1e13eda
basicallynewbie <107107611+basicallynewbie@users.noreply.github.com>
2025-03-30 23:37:53 +0800

    Delete src directory

--- src/fileoperation/newfile.py
+++
@@ -1,9 +0,0 @@
-from tkinter.filedialog import asksaveasfilename
-
-def newFile(jsonpath: str) -> None:
-    jsonpath = asksaveasfilename(defaultextension='json', filetypes=[('Json Files','*.json')])
-    try:
-        with open(jsonpath, 'w') as file:
-            file.write('')
-    except FileNotFoundError:
-        pass
